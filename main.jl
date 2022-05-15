c7c5b8b02f0827f9e54076d22898d490e67ec2ad
Ryu <wowo0118@sasa.hs.kr>
2022-05-16 00:22:17 +0900

    5/16

--- main.jl
+++ main.jl
@@ -6,4 +6,4 @@ using DecisionTree
 R = pf.RF("Data/rgpdata.fasta", "Data/rdata.xls")
 X, Y, L = pf.get_data(R, 12, 'B')
 
-x_train, x_test, y_train, y_test = pf.train_test_split(X, Y, test_size=0.7, random_state=100)
+pf.get_reg_importance(R, X, Y, L, 5, 400)
--- prorf.jl
+++ prorf.jl
@@ -1,6 +1,8 @@
 module prorf
+    using ShapML, DataFrames, DecisionTree
     using PyCall
     using Random
+
     pushfirst!(pyimport("sys")."path", "")
     RFC = pyimport("prorf.rfclass")
 
@@ -20,17 +22,33 @@ module prorf
         return Matrix{Float64}(A), Vector{Float64}(B), Vector{Tuple{Int, Char}}([(C[i, 1], only(C[i, 2])) for i = 1:size(C, 1)])
     end
 
-    function get_reg_importance(x::Matrix{Float64}, y::Vector{Float64}, loc::Vector{Tuple{Int, Char}}, feet::Int, tree::Int; 
-                                split_size::Float64=0.3, )
-        
+    function get_reg_importance(s::RF, x::Matrix{Float64}, y::Vector{Float64}, loc::Vector{Tuple{Int, Char}}, feet::Int, tree::Int; 
+                                val_mode::Bool=false, split_size::Float64=0.3, show_number::Int=20, data_state::Int64=rand(0:typemax(Int64)), learn_state::Int64=rand(0:typemax(Int64)))
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=split_size, random_state=data_state)
+        regr = RandomForestRegressor(n_trees=tree, max_depth=feet, min_samples_leaf=1, rng=learn_state)
+        fit!(regr, x_train, y_train)
+        dx = DataFrame(x, [string(i[1]) for i in loc])
+        rf_importance(regr, dx, show_number=show_number, val_mode=val_mode)
     end
 
-    function train_test_split(x::Matrix{Float64}, y::Vector{Float64}; test_size::Float64, random_state::Int)
+    function rf_dfpredict(regr::RandomForestRegressor, x::DataFrame)
+        return DataFrame(y_pred = predict(regr, Matrix{Float64}(x)))
+    end
+
+    function rf_importance(regr::RandomForestRegressor, dx::DataFrame; show_number::Int=20, val_mode::Bool=false)
+        data_shap = ShapML.shap(explain = dx,
+                        model = regr,
+                        predict_function = rf_dfpredict,
+                        sample_size = 60,
+                        seed = 1)
+    end
+
+    function train_test_split(x::Matrix{Float64}, y::Vector{Float64}; test_size::Float64=0.3, random_state::Int64=rand(0:typemax(Int64)))
         # https://discourse.julialang.org/t/simple-tool-for-train-test-split/473/3
         n = length(y)
         idx = shuffle(MersenneTwister(random_state), 1:n)
         train_idx = view(idx, (floor(Int, test_size*n)+1):n)
         test_idx = view(idx, 1:floor(Int, test_size*n))
-        x[train_idx,:], x[test_idx,:], y[train_idx,:], y[test_idx,:]
+        x[train_idx,:], x[test_idx,:], y[train_idx], y[test_idx]
     end
 end
